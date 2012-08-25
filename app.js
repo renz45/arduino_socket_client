8203914c12795e880c14ea4b48e5a87aebc4ab34
Adam Rensel <adamrensel@gmail.com>
2012-08-25 19:58:39 -0400

    Working :D just need to finish building out functionality after hardware is done

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var arduino = require('duino'),
     board = new arduino.Board();
 
 var io = require('socket.io-client');
-var socket = io.connect('http://localhost:9292');
+var socket = io.connect('http://renz45.arduino_socket_client.nodejitsu.com:80');
 
-socket.on('connect_failed', function(){console.log('Connection Failed');});
+socket.on('connect_failed', function(error){
+  console.log('Connection Failed');
+});
 socket.on('connect', function(){console.log('Connected');});
 socket.on('disconnect', function () {console.log('Disconnected');});
 
