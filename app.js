19c6727fa243de3e7c3364d6e765de082dc83752
Adam Rensel <adamrensel@gmail.com>
2012-08-25 16:29:30 -0400

    This app can now connected to a remote api via a socket to recieve arduino commands

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 var arduino = require('duino'),
     board = new arduino.Board();
 
-var io = require('socket.io').listen(9292);
+var io = require('socket.io-client');
+var socket = io.connect('http://localhost:9292');
 
-io.sockets.on('connection', function (socket) {
+socket.on('connect_failed', function(){console.log('Connection Failed');});
+socket.on('connect', function(){console.log('Connected');});
+socket.on('disconnect', function () {console.log('Disconnected');});
 
-  socket.emit('led_state', { led: 'on'});
-
-  socket.on('command', function (data) {
-    var led = new arduino.Led({
-      board: board,
-      pin: data.pin
-    });
-
-    if(data.state === 'high') {
-      led.on();
-      socket.emit('led_state', { led: 'on'});
-    }else if(data.state === 'low'){
-      led.off();
-      socket.emit('led_state', { led: 'on'});
-    }
+socket.on('command', function (data) {
+  var led = new arduino.Led({
+    board: board,
+    pin: data.pin
   });
+
+  if(data.state === 'high') {
+    led.on();
+    socket.emit('led_state', { led: 'on'});
+  }else if(data.state === 'low'){
+    led.off();
+    socket.emit('led_state', { led: 'on'});
+  }
 });
